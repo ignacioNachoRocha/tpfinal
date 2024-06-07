dd4f626accd6305a59b5d52a198bd0ee1948f6a9
IGNACIO JOSE ROCHA <ignac1997nacho@gmail.com>
2024-06-07 19:22:39 -0300

    agrego el tp

---
+++ planificación de proyecto página/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = mysql.createConnection({
+    
+    host: 'localhost',
+    user: 'root',
+    password: 'ignacio',
+    database: 'proyectoFinal'
+});
+
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Conectado a la base de datos');
+});
+
+app.post('/guardar_turno', (req, res) => {
+    let fecha = req.body.fecha;
+    let hora = req.body.hora;
+    console.log(`Fecha recibida: ${fecha}`);
+    console.log(`Hora recibida: ${hora}`);
+    let checkQuery = `SELECT * FROM turnos WHERE fecha = '${fecha}' AND hora = '${hora}'`;
+
+    db.query(checkQuery, (err, result) => {
+        if (err) throw err;
+        if (result.length > 0) {
+            res.json({ status: 'error', message: 'La fecha ya está seleccionada, por favor elija otra fecha' });
+        } else {
+            let insertQuery = `INSERT INTO turnos (fecha, hora) VALUES ('${fecha}', '${hora}')`;
+            db.query(insertQuery, (err, result) => {
+                if (err) throw err;
+                res.json({ status: 'success', message: 'Turno guardado con éxito' });
+    });
+        }
+    });
+});
+
+server=3003;
+app.listen(server, () => {
+    console.log(`Servidor corriendo en el puerto ${server}`);
+});
